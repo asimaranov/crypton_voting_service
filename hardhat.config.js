f4855830d00817745921a559c706c9355908edb9
asimaranov <a.simaranov@gmail.com>
2022-03-29 19:12:07 +0300

    Changed default network to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,18 @@ require('solidity-coverage');
 require('dotenv').config({path:__dirname+'/.env'})
 
 
+// npx hardhat run --network rinkeby scripts/deploy.js
 
 module.exports = {
   solidity: "0.7.3",
-  defaultNetwork: "rinkeby",
+  defaultNetwork: "hardhat",
   networks: {
     rinkeby: {
       url: process.env.RINKEBY_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
-    }
+    },
+    hardhat: {}
+
   }
 };
 
